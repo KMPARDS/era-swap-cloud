85f641aab4e52a0d882d4de03e75b3012948d99e
Pranjal Agrawal <2018kucp1127@iiitkota.ac.in>
2020-09-24 17:26:22 +0530

    User Data

--- setUP.h
+++ setUP.h
@@ -1,23 +1,24 @@
-# init IPFS on node
+#!/bin/bash
+apt-get update
 wget https://github.com/ipfs/go-ipfs/releases/download/v0.6.0/go-ipfs_v0.6.0_linux-amd64.tar.gz
 tar xvfz go-ipfs_v0.6.0_linux-amd64.tar.gz
 rm go-ipfs_v0.6.0_linux-amd64.tar.gz 
-sudo mv go-ipfs/ipfs /usr/local/bin
+mv go-ipfs/ipfs /usr/local/bin
 rm -rf go-ipfs
 
 # init repo
 echo 'export IPFS_PATH=/data/ipfs' >>~/.bash_profile
 source ~/.bash_profile
-sudo mkdir -p $IPFS_PATH
-sudo chown ubuntu:ubuntu $IPFS_PATH
+mkdir -p $IPFS_PATH
+chown ubuntu:ubuntu $IPFS_PATH
 ipfs init -p server
 
 ipfs config Datastore.StorageMax 20GB
 # uncomment if you want direct access to the instance's gateway
 ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
-#ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
+ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
 
-sudo bash -c 'cat >/lib/systemd/system/ipfs.service <<EOL
+bash -c 'cat >/lib/systemd/system/ipfs.service <<EOL
 [Unit]
 Description=ipfs daemon
 [Service]
@@ -32,7 +33,7 @@ EOL'
 
 
 
-sudo bash -c 'cat > ../../data/ipfs/swarm.key <<EOL
+bash -c 'cat > ../../data/ipfs/swarm.key <<EOL
 /key/swarm/psk/1.0.0/
 /base16/
 96d16ebec087f5e6f3247c50ea9b2f24ac91a17d5b755e7c85071891474c6584
@@ -42,7 +43,7 @@ ipfs bootstrap rm --all
 ipfs bootstrap add /ip4/13.127.174.128/tcp/4001/ipfs/QmepeqPz6xwW5Y8L3Q9FR8BWA9HiArzcRmYZRoCpiUESwX
 
 
-sudo systemctl daemon-reload
-sudo systemctl enable ipfs.service
-sudo systemctl start ipfs
-sudo systemctl status ipfs
+systemctl daemon-reload
+systemctl enable ipfs.service
+systemctl start ipfs
+systemctl status ipfs
