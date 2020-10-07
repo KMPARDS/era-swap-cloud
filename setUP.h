51309e5d3cb59dd44738d27bd449df2ed89bec16
Pranjal Agrawal <2018kucp1127@iiitkota.ac.in>
2020-10-07 09:15:19 +0530

    FINAL

--- setUP.h
+++ setUP.h
@@ -10,7 +10,6 @@ rm -rf go-ipfs
 echo 'export IPFS_PATH=/data/ipfs' >>~/.bash_profile
 source ~/.bash_profile
 mkdir -p $IPFS_PATH
-chown ubuntu:ubuntu $IPFS_PATH
 ipfs init -p server
 
 ipfs config Datastore.StorageMax 20GB
@@ -26,8 +25,6 @@ Description=ipfs daemon
 [Service]
 ExecStart=/usr/local/bin/ipfs daemon --enable-gc
 Restart=always
-User=ubuntu
-Group=ubuntu
 Environment="IPFS_PATH=/data/ipfs"
 [Install]
 WantedBy=multi-user.target
@@ -35,7 +32,7 @@ EOL'
 
 
 
-bash -c 'cat > ../../data/ipfs/swarm.key <<EOL
+bash -c 'cat > /data/ipfs/swarm.key <<EOL
 /key/swarm/psk/1.0.0/
 /base16/
 96d16ebec087f5e6f3247c50ea9b2f24ac91a17d5b755e7c85071891474c6584
@@ -44,8 +41,16 @@ EOL'
 ipfs bootstrap rm --all
 ipfs bootstrap add /ip4/13.127.174.128/tcp/4001/ipfs/QmepeqPz6xwW5Y8L3Q9FR8BWA9HiArzcRmYZRoCpiUESwX
 
+ipfs config Datastore.StorageMax 20GB
+ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
+ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials "[\"true\"]"
+
 
 systemctl daemon-reload
 systemctl enable ipfs.service
 systemctl start ipfs
 systemctl status ipfs
+
+ipfs daemon & 
